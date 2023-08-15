8dfc25f3ad47cb591776fa358e08c42cbeb51dc2
Merxxotas <brayan6590@gmail.com>
2023-08-15 12:54:26 -0500

    adsfas

---
+++ __tests__/ejemplo-objetos.js
@@ -0,0 +1,22 @@
+const cliente = {
+  nombre: "Brayan Marin Guirales",
+  balance: 500,
+};
+
+describe("Testing al Cliente", () => {
+  test("El cliente es premium", () => {
+    expect(cliente.balance).toBeGreaterThan(400);
+  });
+
+  test("Es Brayan Marin Guirales", () => {
+    expect(cliente.nombre).toBe("Brayan Marin Guirales");
+  });
+
+  test("No es otro cliente", () => {
+    expect(cliente.nombre).not.toBe("Brayan");
+  });
+
+  test("No tiene 500", () => {
+    expect(cliente.balance).not.toBe(400);
+  });
+});
